1b5ab2cd209bc69e04bf3a3d0c1d64ebc161c749
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-28 12:08:02 -0500

    updated file

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ Second value: File path we just executed
 */
 
 /*
-In Node.js the 'process' is a global object represent everything going on with this particular app. Similarly to document or window in the browser  
+In Node.js the 'process' is a global object represents everything going on with this particular app. Similarly to document or window in the browser  
 */
 
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
 
 
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
 
 
 // function that can take an array of command-line arguments
@@ -24,7 +24,6 @@ const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i+= 1) {
         console.log(profileDataArr[i]);
     }
-
 
     console.log('================');
 
