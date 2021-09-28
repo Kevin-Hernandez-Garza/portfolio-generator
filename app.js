4768d3ae918648088e0d182e5cb146454cd725f1
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-27 22:34:31 -0500

    capture user input and print output

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Hello Node!');
+/* 
+Displays two elements in a Array
+First value: File path to where Node.js is installed 
+Second value: File path we just executed
+*/
+
+/*
+In Node.js the 'process' is a global object represent everything going on with this particular app. Similarly to document or window in the browser  
+*/
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+// function that can take an array of command-line arguments
+// here we are printing them out one by one 
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i+= 1) {
+        console.log(profileDataArr[i]);
+    }
+
+
+    console.log('================');
+
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
