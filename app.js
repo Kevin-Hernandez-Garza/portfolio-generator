151d9b7d765cc385fa567207da406a3e7da08283
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-29 22:54:54 -0500

    added gitignore file and inquirer prompt method

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-// using the file system module
-const fs = require('fs');
+const inquirer = require('inquirer');
+// // importing the file system module
+// const fs = require('fs');
 
-// we use the require statement to include the generatePage() function 
-// we use the require statement to receive the exported functions 
-const generatePage = require('./src/page-template.js');
+// // we use the require statement to include the generatePage() function 
+// // we use the require statement to receive the exported functions 
+// const generatePage = require('./src/page-template.js');
 
-// array, that hold the user command-line arguments 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // array, that holds the user command-line arguments 
+// // all command line arguments are given to the process in an array called argv (argument values)
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const [name,github] = profileDataArgs; // assignment destructing
+// // const [name,github] = profileDataArgs; // assignment destructing
 
-// generating the html file
-fs.writeFile('index.html', generatePage(name, github), err => {
-    // if there is an error then it will throw an err
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// // generating the html file
+// fs.writeFile('./index.html', pageHTML, err => {
+//     // if there is an error then it will throw an err
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer 
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
