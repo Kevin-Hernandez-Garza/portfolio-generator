13d52f6518a73cafe01e8a82abd2b93d76a7e4b7
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-04 23:03:30 -0500

    generate-file error fixed

--- utils/generate-file.js
+++ utils/generate-file.js
@@ -24,32 +24,23 @@ const writeFile = fileContent => {
     });
 };
 
+// copying file
 const copyFile = () => {
-    return new Promise ((resolve, reject) => {
-        fs.copyFile('./dist/index.html', fileContent, err => {
+    return new Promise((resolve, reject) => {
             fs.copyFile('./src/style.css', './dist/style.css', err => {
                 if (err) {
-                    console.log(err);
+                    reject(err);
                     return;
                 }
                 console.log('Style sheet copied successfully!');
+
+                resolve({
+                    ok:true,
+                    message:'Stylesheet created!'
+                });
             });
-
-
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve({
-                ok:true,
-                message:'File copied!'
-            });
-        });
     });
 };
-
-
 
 //exporting and object with two functions to the app.js file
 // "shorthand property names" 
